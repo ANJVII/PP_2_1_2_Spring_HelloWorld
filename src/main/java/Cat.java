d51bdc0b7b1ebf6f8297ba9dbb4a4d66c89e62fc
Anjela_MV <m.angel.v@mail.ru>
2023-01-29 15:50:59 +0800

    - test passed

---
+++ src/main/java/Cat.java
@@ -0,0 +1,32 @@
+public class Cat {
+
+    private String name;
+    private byte age;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
